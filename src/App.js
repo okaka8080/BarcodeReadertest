135df1a3d693c31a6e61e1cf744c4ae41f519072
okaka8080 <83401443+okaka8080@users.noreply.github.com>
2022-06-16 00:12:40 +0900

    for deploy commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Quagga from "quagga";
+import React from "react";
+
+const btn = document.getElementById("btn");
+const modal = document.getElementById("modal");
+
+btn.addEventListener("click", () => {
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+  Quagga.init(
+    {
+      inputStream: {
+        type: "LiveStream",
+        constraints: {
+          width: window.innerWidth,
+        },
+      },
+      decoder: {
+        readers: [
+          {
+            format: "ean_reader",
+            config: {},
+          },
+        ],
+      },
+    },
+    (err) => {
+      if (!err) {
+        Quagga.start();
+      } else {
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+        Quagga.stop();
+        alert(
+          "この機能を利用するには\nブラウザのカメラ利用を許可してください。"
+        );
+      }
+    }
+  );
+});
+
+Quagga.onDetected((result) => {
+  const code = result.codeResult.code;
+  document.getElementById("code").value = code;
+  modal.style.display = "none";
+  document.body.style.overflow = "";
+  Quagga.stop();
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>バーコード読み込みサンプル</h1>
+      <input id="code" type="text"></input>
+      <button id="btn">カメラでバーコードを読み込む</button>
+      <div id="modal" class="modal">
+        <div id="interactive" class="viewport"></div>
+        <p class="text">カメラにバーコードを写してください。</p>
+      </div>
     </div>
   );
 }
